93727d09e97973c5bc6111cf993586eb4ac7b710
Sarah358 <njerinjuguna358@gmail.com>
2021-08-03 13:09:08 +0300

    added auth tokens

--- app.py
+++ app.py
@@ -1,37 +1,24 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,make_response
 from flask_sqlalchemy import SQLAlchemy
-# from flask_marshmallow import Marshmallow
+from flask_marshmallow import Marshmallow
 import sqlite3
-# from api.utils import generate_sitemap,APIException
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
+import uuid
+import datetime
 import os
+import jwt
+import datetime
+from functools import wraps
+from werkzeug.security import generate_password_hash,check_password_hash
 
 # init app
 app = Flask(__name__)
 
-# Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET') # Change this!
-jwt = JWTManager(app)
+# Setup the secret-key
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') # Change this!
 
-# access token
-# Create a route to authenticate your users and return JWTs. The
-# create_access_token() function is used to actually generate the JWT.
-@app.route("/token", methods=["POST"])
-def create_token():
-    name = request.json.get("name", None)
-    password = request.json.get("password", None)
-    if name != "test" or password != "test":
-        return jsonify({"msg": "Bad name or password"}), 401
 
-    access_token = create_access_token(identity=name)
-    return jsonify(access_token=access_token)
+
     
-
-
-
 
 # setting sqlalchemy db uri
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -44,7 +31,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # init marshmallow
-# ma = Marshmallow(app)
+ma = Marshmallow(app)
 
 # user model
 class User(db.Model):
@@ -60,41 +47,48 @@ class User(db.Model):
         self.email = email
         self.password = password
 
-# user shema
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('id','name','email','password')
+# creating decorator to work with access token
+def token_required(f):
+    @wraps(f)
+    # pass positional arguments and keyword arguments
+    def decorated(*args,**kwargs):
+        token = None
 
-# init schema
-# user_schema = UserSchema(strict = True)
-# users_schema = UserSchema(many = True, strict = True)
+        # pass token in the header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message':'Token is missing'}),401
+        # try catch errors
+        try:
+            data = jwt.decode(token,app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(id= data['id'] ).first()
+        except:
+            return jsonify({'message':'Token is invalid'}),401
+        # pass user object to the route
+        return f(current_user, *args,**kwargs)
+    # return decorated fuunction
+    return decorated
+
 
 # create a user
-@app.route('/users',methods= ['POST'])
-def add_user():
-    name = request.json['name']
-    email = request.json['email']
-    password = request.json['password']
-
-    conn = sqlite3.connect('db.sqlite')
-    conn_db = conn.cursor()
-    conn_db.execute("SELECT * FROM user where email = ? AND password = ?;",(email,password))
-    record = conn_db.fetchall()
-    if(len(record) == 0):
-        new_user = User(name,email,password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        response ='User account created'
-        return response
-    else:
-        response = "User details already exists"
-        return response
-
+@app.route('/user',methods= ['POST'])
+# passing the decorator
+@token_required
+def add_user(current_user):
+    data = request.get_json()
+    hashed_password = generate_password_hash(data.get('password'), method='sha256')
+    new_user = User(name=data.get('name'),email=data.get('email'), password=hashed_password)       
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 201
+  
+   
 
 # get users
-@app.route('/users',methods = ['GET'])
-def get_user():
+@app.route('/user',methods = ['GET'])
+@token_required
+def get_user(current_user):
     users = User.query.all()
     output = []
     for user in users:
@@ -106,6 +100,67 @@ def get_user():
         output.append(user_data)
 
     return jsonify({"users":output})
+
+# get one user
+@app.route('/user/<id>',methods =['GET'])
+@token_required
+
+def get_one_user(current_user,id):
+    user = User.query.filter_by(id = id).first()
+
+    if not user:
+        return jsonify ({'message':'No user found'})
+    
+    user_data = {}
+    user_data['id'] = user.id
+    user_data['name'] = user.name
+    user_data['email'] = user.email
+    user_data['password'] = user.password
+    
+    return jsonify ({'user':user_data})
+
+# edit user
+@app.route('/user/<id>',methods = ['PUT'])
+def edit_user(id):
+    return ''
+
+# delete user
+@app.route('/user/<id>',methods = ['DELETE'])
+@token_required
+def delete_user(current_user,id):
+    user = User.query.filter_by(id = id).first()
+    if not user:
+        return jsonify ({'message':'No user found'})
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify ({'message':'user deleted'})
+
+
+# login
+@app.route('/login')
+
+
+def login():
+    # authentication
+    auth = request.authorization
+    # check for authentication before logging in
+    if not auth or not auth.username or not auth.password:
+        return make_response('could not verify',401,{'WWW-Authenticate':'Basic realm = "login required"'})
+
+    # query user
+    user = User.query.filter_by(name = auth.username).first()
+
+    if not user:
+        return jsonify ({'message':'no user found'})
+    if check_password_hash(user.password,auth.password):
+        # encode the token in the secret key
+        token = jwt.encode({'id':user.id,'exp': datetime.datetime.utcnow() +datetime.timedelta(minutes = 30)},app.config['SECRET_KEY'])
+        
+        return jsonify ({'token':token}),200
+    return make_response('could not verify password',401,{'WWW-Authenticate':'Basic realm = "login required"'})
+
+
 
 
 # run server
